bed29e33e50847e998274b7e4fd2ce3c55853308
Darko <darkoatanasovski@gmail.com>
2023-04-11 00:02:22 +0200

    disable fs for browsers

--- next.config.js
+++ next.config.js
@@ -4,6 +4,67 @@ const nextConfig = {
 		appDir: true,
 		runtime: "edge",
 	},
+	images: {
+		domains: ["nlidwaakjqdqreffanve.supabase.co"],
+	},
+	async headers() {
+		return [
+			{
+				source: "/collection/:id",
+				headers: [
+					{
+						key: "Cache-Control",
+						value: "public, s-maxage=1200, stale-while-revalidate=600",
+					},
+				],
+			},
+			{
+				source: "/(.*)",
+				headers: securityHeaders,
+			},
+		];
+	},
 };
+
+const ContentSecurityPolicy = `
+    default-src 'self' vercel.live;
+    script-src 'self' 'unsafe-eval' 'unsafe-inline' cdn.vercel-insights.com vercel.live;
+    style-src 'self' 'unsafe-inline';
+    img-src * blob: data:;
+    media-src 'none';
+    connect-src *;
+    font-src 'self';
+`;
+
+const securityHeaders = [
+	{
+		key: "Content-Security-Policy",
+		value: ContentSecurityPolicy.replace(/\n/g, ""),
+	},
+	{
+		key: "Referrer-Policy",
+		value: "origin-when-cross-origin",
+	},
+	{
+		key: "X-Frame-Options",
+		value: "DENY",
+	},
+	{
+		key: "X-Content-Type-Options",
+		value: "nosniff",
+	},
+	{
+		key: "X-DNS-Prefetch-Control",
+		value: "on",
+	},
+	{
+		key: "Strict-Transport-Security",
+		value: "max-age=31536000; includeSubDomains; preload",
+	},
+	{
+		key: "Permissions-Policy",
+		value: "camera=(), microphone=(), geolocation=()",
+	},
+];
 
 module.exports = nextConfig;
